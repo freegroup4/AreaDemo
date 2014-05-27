8ca404845ec2e126b7964ae7ee85306b19283240
freegroup4 <freegroup4@yopmail.com>
2014-05-27 10:28:36 +0800

    Final Commit
    Final

--- src/areademo/AreaDemo.java
+++ src/areademo/AreaDemo.java
@@ -1,22 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package areademo;
-
-/**
- *
- * @author Chen-USE
- */
-public class AreaDemo {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+class CRectangle{
+    int width;
+    int height;
+    int area(){
+        return width*height;
     }
-    
+}
+public class AreaDemo {
+    public static void main(String[] args) {
+        CRectangle c1 = new CRectangle();
+        c1.width = 55; c1.height = 22;
+        System.out.println(c1.area());
+    }
 }
